80c1b73474066dc6ef115937d8645f50f5988c1a
lexash707 <lexashart@gmail.com>
2024-01-18 23:52:13 +0100

    initial commit

---
+++ src/main/java/com/example/review/gamereviewservice/GameReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.review.gamereviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameReviewServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/review/gamereviewservice/GameReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.review.gamereviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
